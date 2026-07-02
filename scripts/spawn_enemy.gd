ed25ee6e1815d2613630a44618bb5d3c0428503f
Icaleb1 <calebfernandes080@gmail.com>
2026-07-02 06:36:15 -0300

    feat: add automatic enemy spawner

---
+++ scripts/spawn_enemy.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+const PRE_ALIEN = preload("res://scenes/alien_1.tscn")
+
+func _ready() -> void:
+	start_timer()
+
+
+
+
+func _on_timer_timeout() -> void:
+	var alien_clone = PRE_ALIEN.instantiate()
+	get_parent().add_child(alien_clone)
+	alien_clone.global_position = Vector2(360,randi_range(20,120))
+	start_timer()
+
+func start_timer() -> void:
+	$Timer.wait_time = randi_range(1,3)
+	$Timer.start()
