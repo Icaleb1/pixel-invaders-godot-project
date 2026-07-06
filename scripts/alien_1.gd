1b81e0bb2cad05bf1745c70ce1356ffd9064a641
Icaleb1 <calebfernandes080@gmail.com>
2026-07-06 07:06:45 -0300

    fix: prevent duplicate points from a single collision

--- scripts/alien_1.gd
+++ scripts/alien_1.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
 @onready var animation:AnimatedSprite2D = $Anim
-var vel:int = 60
+var vel:int = 80
+var life:bool = true
 
 @onready var sound_dead:AudioStreamPlayer = $SoundDead
 
@@ -12,11 +13,13 @@ func _process(delta: float) -> void:
 	translate(Vector2(-1,0) * vel * delta)
 
 func _on_area_entered(_area) -> void:
-	vel = 0
-	animation.play("dead")
-	sound_dead.play()
-	await animation.animation_finished
-	kill()
+	if life:
+		vel = 0
+		life = false
+		animation.play("dead")
+		sound_dead.play()
+		await animation.animation_finished
+		kill()
 	
 		
 func kill() -> void:
